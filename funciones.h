d6848f54726857d3b83e6f162b7117cbf4a7636e
IvethCastro <iveth.castro@udla.edu.ec>
2026-01-18 19:17:36 -0500

    Update pollution limits and add climate data structure

--- funciones.h
+++ funciones.h
@@ -2,7 +2,7 @@
 #define FUNCIONES_H
 
 #define LIM_PM25 15
-#define LIM_SO2 40
+#define LIM_SO2 25
 #define LIM_NO2 40
 
 
@@ -28,28 +28,34 @@ typedef struct {
 } Proyeccion;
 
 typedef struct {
+    float temperatura;
+    float viento;
+    float humedad;
+} Clima;
+
+
+typedef struct {
     char nombre[40];
     Historial historial;
+    Contaminacion actual;
     Proyeccion proy;
+    Clima clima;
     int hayPrediccion;
+    int tieneDatos;
+    int tieneClima;
 } Zona;
 
-/* ================= VALIDACIONES ================= */
 
 int leerEnteroConRango(int inicio, int fin);
 float leerFlotanteConRango(float inicio, float fin);
-
-/* ================= INICIALIZACIÓN Y ARCHIVOS ================= */
-
 void inicializarZonas(Zona zonas[]);
 void cargarDatosHistoricos(Zona zonas[]);
 void guardarDatosHistoricos(Zona zonas[]);
 void guardarPrediccion(Zona zonas[], int id);
 
-/* ================= UTILIDADES ================= */
-
 void mostrarZonas(Zona zonas[]);
 void monitoreoActual(Zona *z);
+void mostrarDatosActuales(Zona zonas[]);
 
 float promedioPonderado(float valores[]);
 float factorClimatico(float temp, float viento, float humedad);
@@ -57,17 +63,15 @@ float factorClimatico(float temp, float viento, float humedad);
 const char* interpretacionAQI(float aqi);
 const char* nivelCO2Exterior(float c);
 
-/* ================= AQI ================= */
 
 float calcularAQI(float C, float Clow, float Chigh, float Ilow, float Ihigh);
 float aqiPM25(float c);
 float aqiSO2(float c);
 float aqiNO2(float c);
 
-/* ================= OPCIONES DEL MENÚ ================= */
 
 void ingresarContaminacionActual(Zona zonas[]);
-void prediccion(Zona zonas[], int id);
+void prediccion(Zona zonas[]);
 void alertasRecomendaciones(Zona zonas[]);
 void promediosHistoricos(Zona zonas[]);
 void exportarReporte(Zona zonas[]);
