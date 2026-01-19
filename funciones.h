47da49b2e02d535960c4222488d300e223429f9b
IvethCastro <iveth.castro@udla.edu.ec>
2026-01-18 20:38:11 -0500

    Refactor funciones.h to add new functions and limits

--- funciones.h
+++ funciones.h
@@ -1,13 +1,16 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
+#include <stdio.h>
+
 #define LIM_PM25 15
 #define LIM_SO2 25
 #define LIM_NO2 40
 
-
-#define ZONAS 5
 #define DIAS 30
+#define MAX_ZONAS 20
+
+
 
 typedef struct {
     float pm25;
@@ -33,7 +36,6 @@ typedef struct {
     float humedad;
 } Clima;
 
-
 typedef struct {
     char nombre[40];
     Historial historial;
@@ -48,33 +50,40 @@ typedef struct {
 
 int leerEnteroConRango(int inicio, int fin);
 float leerFlotanteConRango(float inicio, float fin);
-void inicializarZonas(Zona zonas[]);
-void cargarDatosHistoricos(Zona zonas[]);
-void guardarDatosHistoricos(Zona zonas[]);
-void guardarPrediccion(Zona zonas[], int id);
+int soloLetras(char *cadena);
 
-void mostrarZonas(Zona zonas[]);
-void monitoreoActual(Zona *z);
-void mostrarDatosActuales(Zona zonas[]);
+
+void mostrarZonas(Zona zonas[], int totalZonas);
+void anadirZona(Zona zonas[], int *totalZonas);
+
+
+void cargarDatosHistoricos(Zona zonas[], int totalZonas);
+void guardarDatosHistoricos(Zona zonas[], int totalZonas);
+void cargarZonas(Zona zonas[], int *totalZonas);
+
+
+void ingresarContaminacionActual(Zona zonas[], int totalZonas);
+void mostrarDatosActuales(Zona zonas[], int totalZonas);
+
+
 
 float promedioPonderado(float valores[]);
 float factorClimatico(float temp, float viento, float humedad);
-
-const char* interpretacionAQI(float aqi);
-const char* nivelCO2Exterior(float c);
-
 
 float calcularAQI(float C, float Clow, float Chigh, float Ilow, float Ihigh);
 float aqiPM25(float c);
 float aqiSO2(float c);
 float aqiNO2(float c);
 
+const char* interpretacionAQI(float aqi);
+const char* nivelCO2Exterior(float c);
 
-void ingresarContaminacionActual(Zona zonas[]);
-void prediccion(Zona zonas[]);
-void alertasRecomendaciones(Zona zonas[]);
-void promediosHistoricos(Zona zonas[]);
-void exportarReporte(Zona zonas[]);
+void prediccion(Zona zonas[], int totalZonas);
+void alertasRecomendaciones(Zona zonas[], int totalZonas);
+void promediosHistoricos(Zona zonas[], int totalZonas);
+void exportarReporte(Zona zonas[], int totalZonas);
+void monitoreoActual(Zona *z);
+
 
 #endif
- 
+
